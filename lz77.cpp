9358fec3df0a2470c7f82bfe42e0fc8c61a0283d
Parikshit Misra <perryizgr8@live.in>
2016-11-01 19:02:22 +0530

    i have typed things

--- lz77.cpp
+++ lz77.cpp
@@ -5,9 +5,16 @@
 #include <fstream>
 #include <string>
 #include <sstream>
+#include "aho_corasick.hpp"
 
 #define MAX_STR_LEN 16 //or 15?
-#define MAX_CHAR_JUMP 16 //or 15?
+#define WINDOW_LEN 64 //or 15?
+
+std::string get_word(int c, std::string text)
+{
+	//find word starting at position c
+
+}
 
 int main()
 {
@@ -15,6 +22,7 @@ int main()
 	std::ifstream file("wiki.txt");
 	std::stringstream buffer;
 	buffer << file.rdbuf();
+	std::string full_text;
 
 	//extract chars
 	while (!buffer.eof())
@@ -23,7 +31,8 @@ int main()
 		buffer.get(c);
 		if (!buffer.eof())
 		{
-			std::cout << c;
+			//std::cout << c;
+			full_text.append(1u, c);
 		}
 	}
 
@@ -31,5 +40,28 @@ int main()
 	//1. character window
 	//2. substring searching within window
 	//3. writing to another buffer instead of screen
+
+	std::string output, window;
+	int chars = 0;
+	for (auto c : full_text) {
+		window.append(&c);
+		chars++;
+		if (c == ' ') {
+			//get next word
+			std::string next_word = get_word(chars, full_text);
+		}
+		if (window.length() % WINDOW_LEN == 0) {
+			//copy window to output, empty window
+			output.append(window);
+			window.clear();
+		}
+	}
+	aho_corasick::trie trie;
+	trie.insert("hers");
+	trie.insert("his");
+	trie.insert("she");
+	trie.insert("he");
+	//std::vector<aho_corasick::emit<char>> result = trie.parse_text("he");
+	auto result = trie.parse_text("he he he he he");
     return 0;
 }
